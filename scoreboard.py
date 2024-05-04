ee85b6a171ec844ceb4d0bd9150f59a4e7c8975e
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-04 17:46:27 +0300

    finished setting up the scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    """Tracks and Displays the Score for Both Players"""
+    def __init__(self):
+        super().__init__()
+        self.l_score = 0
+        self.r_score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+
+    def scoreboard(self):
+        """Displays the current score on the screen"""
+        self.clear()
+
+        # Display left score
+        self.goto(-100, 220)  # Adjust offset for better positioning
+        self.write(self.l_score, align="center", font=("courier", 50, "normal"))
+
+        # Display right score
+        self.goto(100, 220)  # Adjust offset for better positioning
+        self.write(self.r_score, align="center", font=("courier", 50, "normal"))
+
+    def update_r(self):
+        """Updates the right player's score and displays the scoreboard"""
+        self.r_score += 1
+        self.scoreboard()
+
+    def update_l(self):
+        """Updates the left player's score and displays the scoreboard"""
+        self.l_score += 1
+        self.scoreboard()
