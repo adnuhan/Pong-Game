14c05ae12f998a5e75a03147e16d28faf328a7c3
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-05 20:35:31 +0300

    Defines a Ball that Moves and Responds to Collisions with Walls and Paddles

--- ball.py
+++ ball.py
@@ -6,31 +6,29 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.color("white")
+        self.color("White")
         self.penup()
-        self.goto(0, 0)
-        self.x_axis = 10
-        self.y_axis = 10
+        self.x_move = 10
+        self.y_move = 10
         self.ball_speed = 0.1
 
     def move(self):
         """Moves the ball by updating its x and y coordinates"""
-        x = self.xcor() + self.x_axis
-        y = self.ycor() + self.y_axis
-        self.goto(x, y)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
     def bounce_y(self):
-        """Handles the Ball Bouncing off Top/Bottom Wall with Gradual Speed Increase"""
-        self.y_axis *= -1
-        self.ball_speed *= 1.1
+        """Handles the Ball Bouncing off Top/Bottom Wall"""
+        self.y_move *= -1
 
     def bounce_x(self):
         """Handles the Ball Bouncing off the Left/Right sides with Gradual Speed Increase"""
-        self.x_axis *= -1
-        self.ball_speed *= 1.1
+        self.x_move *= -1
+        self.ball_speed *= 0.9
 
     def rest_position(self):
         """Rests the Ball Position and Speed"""
         self.goto(0, 0)
-        self.bounce_x()
         self.ball_speed = 0.1
+        self.bounce_x()
