aa5c9e4aaa482328ca965d7a4d2ad3a8295a971b
Nahunda Bekele <138674654+NahundaBekele@users.noreply.github.com>
2024-05-05 20:59:00 +0300

    Tracks and Displays the Score for Both Players

--- scoreboard.py
+++ scoreboard.py
@@ -5,11 +5,11 @@ class Scoreboard(Turtle):
     """Tracks and Displays the Score for Both Players"""
     def __init__(self):
         super().__init__()
-        self.l_score = 0
-        self.r_score = 0
-        self.color("white")
+        self.color("White")
         self.penup()
         self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
 
     def scoreboard(self):
         """Displays the current score on the screen"""
@@ -23,12 +23,12 @@ class Scoreboard(Turtle):
         self.goto(100, 220)  # Adjust offset for better positioning
         self.write(self.r_score, align="center", font=("courier", 50, "normal"))
 
-    def update_r(self):
-        """Updates the right player's score and displays the scoreboard"""
-        self.r_score += 1
-        self.scoreboard()
-
     def update_l(self):
         """Updates the left player's score and displays the scoreboard"""
         self.l_score += 1
+        self.scoreboard()
+
+    def update_r(self):
+        """Updates the right player's score and displays the scoreboard"""
+        self.r_score += 1
         self.scoreboard()
